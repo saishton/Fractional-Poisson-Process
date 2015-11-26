841e83fbbc1d929e2fb5230fd45451b9474942d6
Stephen <saishton@gmail.com>
2015-11-26 16:32:18 +0000

    Added network dynamic visualisation

--- create_avi.m
+++ create_avi.m
@@ -19,11 +19,14 @@ parfor n=1:num_people
     coords(n,:) = scale*[sin(n*theta) cos(n*theta)];
 end
 
+node_freq = zeros(num_people,1);
+line_freq = zeros(num_people);
 clustering = zeros(1,num_times);
 map(num_times) = struct('cdata',[],'colormap',[]);
 links(num_times) = struct('cdata',[],'colormap',[]);
 degdist(num_times) = struct('cdata',[],'colormap',[]);
 for m=1:num_times
+    
     adj = zeros(num_people);
     current_time = (m-1)*contact_time;
     
@@ -46,6 +49,10 @@ for m=1:num_times
     else
         clustering(m) = trace(adj3)/(adj2sum - trace(adj2));
     end
+    node_freq = node_freq+sum(adj,2);
+    rel_node_freq = node_freq/max(node_freq);
+    line_freq = line_freq+adj;
+    rel_line_freq = line_freq/(max(max(line_freq)));
        
     %==Create Video Frames==%
     map_fig = figure();
@@ -54,10 +61,41 @@ for m=1:num_times
     text(0,-1.2*scale,str);
     axis([-1.5 1.5 -1.5 1.5]*scale);
     axis off;
+    set(gcf,'color','w');
     map(m) = getframe(map_fig);
     close(map_fig);
     
-%LINKS
+    links_fig = figure();
+    link_size = rel_node_freq*50;
+    tempadj = logical(rel_line_freq);
+    [row,col] = find(tempadj);
+    tempcoords = zeros(num_people,2);
+    hold on
+    for i=1:length(row)
+        thisrow = row(i);
+        thiscol = col(i);
+        if thisrow >= thiscol
+            line_col = (1-rel_line_freq(thisrow,thiscol))*[1 1 1];
+            x1 = coords(thisrow,1);
+            y1 = coords(thisrow,2);
+            x2 = coords(thiscol,1);
+            y2 = coords(thiscol,2);
+            line([x1 x2],[y1 y2],'Color',line_col)
+        end
+        tempcoords(i,:) = coords(thisrow,:);
+    end
+    tempcoords = tempcoords(any(tempcoords,2),:);
+    tempcoords = unique(tempcoords,'rows','stable');
+    link_size(link_size==0) = [];
+    scatter(tempcoords(:,1),tempcoords(:,2),link_size,'filled')
+    hold off
+    str = sprintf('Time: %d', current_time);
+    text(0,-1.2*scale,str);
+    axis([-1.5 1.5 -1.5 1.5]*scale);
+    axis off;
+    set(gcf,'color','w');
+    links(m) = getframe(links_fig);
+    close(links_fig);
 
     degdist_fig = figure();
     edges = -0.5:1:5.5;
@@ -78,10 +116,10 @@ open(v)
 writeVideo(v,map)
 close(v)
 
-% v = VideoWriter(linksfilename);
-% open(v)
-% writeVideo(v,links)
-% close(v)
+v = VideoWriter(linksfilename);
+open(v)
+writeVideo(v,links)
+close(v)
 
 v = VideoWriter(degdistfilename);
 open(v)
